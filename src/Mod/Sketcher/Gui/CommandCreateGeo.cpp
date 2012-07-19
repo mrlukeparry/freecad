93941a13418f5dafd7e7ebee967dc242cd9a6606
mrlukeparry <l.parry@warwick.ac.uk>
2012-07-19 20:59:53 +0100

    Fixed a sketcher error that seems to be cropping up. Ignore CommandCreateGeo.cpp

--- src/Mod/Sketcher/Gui/CommandCreateGeo.cpp
+++ src/Mod/Sketcher/Gui/CommandCreateGeo.cpp
@@ -919,11 +919,9 @@ public:
             float angle = atan2f(dy_ , dx_) * 180 / M_PI;
 
             char buf[40];
-<<<<<<< HEAD
-            sprintf( buf, "R(%.1f),%.1f ", radius, angle);
-=======
+
             sprintf( buf, " (R%.1f,%.1f)", radius, angle);
->>>>>>> dfca264acfa6fc7c58d4ca695c5ac76507568073
+
             std::string text = buf;
             setPositionText(onSketchPos, text);
 
@@ -944,19 +942,13 @@ public:
                 float dy = rx * sin(angle) + ry * cos(angle);
                 EditCurve[i] = Base::Vector2D(CenterPoint.fX + dx, CenterPoint.fY + dy);
             }
-<<<<<<< HEAD
-=======
-
->>>>>>> dfca264acfa6fc7c58d4ca695c5ac76507568073
             // Display radius and end angle
             float radius = (onSketchPos - EditCurve[0]).Length();
 
             char buf[40];
-<<<<<<< HEAD
-            sprintf( buf, "R(%.1f),%.1f ", radius, arcAngle * 180 / M_PI);
-=======
+
             sprintf( buf, " (R%.1f,%.1f)", radius, arcAngle * 180 / M_PI);
->>>>>>> dfca264acfa6fc7c58d4ca695c5ac76507568073
+
             std::string text = buf;
             setPositionText(onSketchPos, text);
             
@@ -1143,10 +1135,7 @@ public:
 
     virtual void mouseMove(Base::Vector2D onSketchPos)
     {
-<<<<<<< HEAD
 
-=======
->>>>>>> dfca264acfa6fc7c58d4ca695c5ac76507568073
         if (Mode==STATUS_SEEK_First) {
             setPositionText(onSketchPos);
             if (seekAutoConstraint(sugConstr1, onSketchPos, Base::Vector2D(0.f,0.f))) {
@@ -1170,11 +1159,9 @@ public:
             float radius = (onSketchPos - EditCurve[0]).Length();
 
             char buf[40];
-<<<<<<< HEAD
+
             sprintf( buf, "R(%.1f)", radius);
-=======
-            sprintf( buf, "R%.1f", radius);
->>>>>>> dfca264acfa6fc7c58d4ca695c5ac76507568073
+
             std::string text = buf;
             setPositionText(onSketchPos, text);
 
